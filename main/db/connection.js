221577879c19a9236a913943287dfdc0cc51f49e
jimmyboy722 <jeckman722@gmail.com>
2024-09-17 23:46:53 -0400

    created .env and .gitignore files, added to schema and configure file

--- main/db/connection.js
+++ main/db/connection.js
@@ -1,12 +1,12 @@
-// IMPORTING POOL CLASS WITH REQUIRE METHOD //
+// IMPORTING THE PG MODULE AND EXTRACTING THE POOL CLASS FOR INTERACTING WITH THE POSTGRESQL DATABASE//
 const { Pool } = require("pg");
 
 // CREATING NEW POOL CLASS/OBJECT //
 const pool = new Pool({
-  user: "postgres",
-  password: "Highvibes516!",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   host: "localhost",
   database: "company",
 });
-
+// EXPORTING THE POOL INSTANCE
 module.exports = pool;
