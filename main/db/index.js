3a0621f9369c8979f57e8970e56e885a3693fb5c
jimmyboy722 <jeckman722@gmail.com>
2024-09-22 01:00:57 -0400

    adding db queries to the index.js file in the db directory

--- main/db/index.js
+++ main/db/index.js
@@ -21,17 +21,62 @@ class DATABASE {
       "SELECT department.id, department.name FROM department"
     );
   }
-  // FUNCITON TO VIEW ALL EMPLOYEES PER ACCEPTANCE CRITERIA
+  // FUNCITON TO VIEW ALL EMPLOYEES PER ACCEPTANCE CRITERIA AND JOIN WITH ROLE TABLE AND DEPARTMENT TABLE
   viewAllEmployees() {
     return this.query(
       //SQL STATEMENT
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_id;"
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT (manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
+    );
+  }
+  // ADDING EMPLOYEES
+  addEmployee(employee) {
+    const { first_name, last_name, role_id, manager_id } = employee;
+    return this.query(
+      "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)",
+      [first_name, last_name, role_id, manager_id]
+    );
+  }
+  // REMOVING EMPLOYEES WITH ID'S
+  deleteEmployee(employeeId) {
+    return this.query("DELETE FROM employee WHERE id = $1", [employeeId]);
+  }
+  // UPDATING EMPLOYEE ROLE
+  updateEmployeeRole(employeeId, roleId) {
+    return this.query("UPDATE employee SET role_id = $1 WHERE id = $2", [
+      roleId,
+      employeeId,
+    ]);
+  }
+  // UPDATING EMPLOYEE MANAGERS
+  updateEmployeeManager(employeeId, managerId) {
+    return this.query("UPDATE employee SET manager_id = $1 WHERE id = $2", [
+      managerId,
+      employeeId,
+    ]);
+  }
+  // FIND ALL MANAGERS
+  viewAllManagers(employeeId) {
+    return this.query(
+      "SELECT id, first_name, last_name FROM employee WHERE id != $1"
     );
   }
   // FUNCTION TO VIEW ALL ROLES PER ACCEPTANCE CRITERIA
   viewAllRoles() {
     return this.query(
-      "SELECT role.id, role.job_title, role.salary FROM role LEFT JOIN department on role.department_id = department.id, department.name AS department; "
+      "SELECT role.id, role.job_title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
     );
   }
+  // ADDING ROLES
+  addRole(role) {
+    const { title, salary, department_id } = role;
+    return this.query(
+      "INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)",
+      [title, salary, department_id]
+    );
+  }
+  // DELETING ROLES
+  deleteRole(roleId) {
+    return this.query("DELETE FROM role WHERE id = $1", [roleId]);
+  }
+  // FINDING DEPARTMENTS SALARIES
 }
